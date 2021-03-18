2683d74a7ffab30420ad320a4eb0f776808f3c02
MingYang-Yin <64779177+MingYang-Yin@users.noreply.github.com>
2021-03-18 04:52:27 -0800

    Add files via upload
    这些文件位于项目的app文件夹之下

---
+++ src/main/java/com/example/myapplication/FirstActivity.kt
@@ -0,0 +1,11 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FirstActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_f_i_r_s_t)
+    }
+}
